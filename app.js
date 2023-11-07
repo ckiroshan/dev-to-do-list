4afc84cd2eed8447a72f5ab26e22528e2016781a
Your <ckiroshan@gmail.com>
2023-11-07 22:58:12 +0530

    Saved item deletion to local storage.

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     // Animation
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -140,4 +141,19 @@ function getTodos(todo) {
   });
 }
 
+function removeLocalTodos(todo) {
+  // Check if Todos already exist
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
 
+  // Get relevant Todo index
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  // Remove from Local storage
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+}
